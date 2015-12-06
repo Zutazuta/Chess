1300bbb2c45f87ebf15acca8a2b7da9d8e8c20b4
Thomas Rude <Thomas Rude>
2015-12-05 22:16:45 -0800

    Add move class

--- src/chess/components/pieces/Piece.java
+++ src/chess/components/pieces/Piece.java
@@ -1,9 +1,13 @@
 package chess.components.pieces;
 
+import java.util.List;
+
 public class Piece {
 
 	private String color;
 	private String type;
+	
+	private List<Move> availableMoves;
 
 	public Piece(String color, String type) {
 		this.color = color;
