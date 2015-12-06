10d423154fbef5b6e3449edbceae0448f64d9cef
Thomas Rude <Thomas Rude>
2015-12-05 17:52:24 -0800

    More pieces

--- src/chess/components/Board.java
+++ src/chess/components/Board.java
@@ -13,14 +13,33 @@ import chess.components.pieces.Piece;
 
 public class Board {
 	
-	JPanel boardFrame;
-	JPanel[][] boardSquares;
+	private JPanel boardFrame;
+	private JPanel[][] boardSquares;
 	
-	public Board() {
+	//The white pieces
+	private JLabel wKing;
+	private JLabel wQueen;
+	private JLabel wBishop;
+	private JLabel wKnight;
+	private JLabel wRook;
+	private JLabel wPawn;
+	
+	//The black pieces
+	private JLabel bKing;
+	private JLabel bQueen;
+	private JLabel bBishop;
+	private JLabel bKnight;
+	private JLabel bRook;
+	private JLabel bPawn;
+	
+	public Board() throws IOException {
 		boardFrame = new JPanel(new GridLayout(0, 8));
     	boardSquares = new JPanel[8][8];
     	boardFrame.setBorder(new LineBorder(Color.BLACK));
+    	generateImages();
 	}
+	
+	
 
     public JPanel drawBoard(Piece[][] boardState) throws IOException {    	
 
@@ -30,22 +49,56 @@ public class Board {
 
                 if ((j % 2 == 1 && i % 2 == 1) || (j % 2 == 0 && i % 2 == 0)) {
                 	square.setBackground(Color.WHITE);
-                	BufferedImage image = ImageIO.read(new File("art/wRook.png"));
-                	JLabel label = new JLabel(new ImageIcon(image));
-                	label.setSize(new Dimension(64, 64));
-                	square.add(label);
-                } else {
-                	square.setBackground(Color.BLACK);
-                	BufferedImage image = ImageIO.read(new File("art/bRook.png"));
-                	JLabel label = new JLabel(new ImageIcon(image));
-                	label.setSize(new Dimension(64, 64));
-                	square.add(label);
                 	
+                } else {
+                	square.setBackground(Color.BLACK);                	
                 }
                 boardFrame.add(square);
             }           
         }
         
         return boardFrame;
+    }
+    
+    private JLabel determinePieceImage(Piece piece) {
+    	
+    }
+    
+    private void generateImages() throws IOException {
+    	wKing = new JLabel(new ImageIcon(ImageIO.read(new File("art/wKing.png"))));
+    	wKing.setSize(new Dimension(64, 64));
+    	
+    	wQueen = new JLabel(new ImageIcon(ImageIO.read(new File("art/wQueen.png"))));
+    	wQueen.setSize(new Dimension(64, 64));
+    	
+    	wBishop = new JLabel(new ImageIcon(ImageIO.read(new File("art/wBishop.png"))));
+    	wBishop.setSize(new Dimension(64, 64));
+    	
+    	wKnight = new JLabel(new ImageIcon(ImageIO.read(new File("art/wKnight.png"))));
+    	wKnight.setSize(new Dimension(64, 64));
+    	
+    	wRook = new JLabel(new ImageIcon(ImageIO.read(new File("art/wRook.png"))));
+    	wRook.setSize(new Dimension(64, 64));
+    	
+    	wPawn = new JLabel(new ImageIcon(ImageIO.read(new File("art/wPawn.png"))));
+    	wPawn.setSize(new Dimension(64, 64));
+    	
+    	bKing = new JLabel(new ImageIcon(ImageIO.read(new File("art/bKing.png"))));
+    	bKing.setSize(new Dimension(64, 64));
+    	
+    	bQueen = new JLabel(new ImageIcon(ImageIO.read(new File("art/bQueen.png"))));
+    	bQueen.setSize(new Dimension(64, 64));
+    	
+    	bBishop = new JLabel(new ImageIcon(ImageIO.read(new File("art/bBishop.png"))));
+    	bBishop.setSize(new Dimension(64, 64));
+    	
+    	bKnight = new JLabel(new ImageIcon(ImageIO.read(new File("art/bKnight.png"))));
+    	bKnight.setSize(new Dimension(64, 64));
+    	
+    	bRook = new JLabel(new ImageIcon(ImageIO.read(new File("art/bRook.png"))));
+    	bRook.setSize(new Dimension(64, 64));
+    	
+    	bPawn = new JLabel(new ImageIcon(ImageIO.read(new File("art/bPawn.png"))));
+    	bPawn.setSize(new Dimension(64, 64));
     }
 }
--- src/chess/gui/GraphicalUserInterface.java
+++ src/chess/gui/GraphicalUserInterface.java
@@ -19,20 +19,22 @@ public class GraphicalUserInterface {
 
             @Override
             public void run() {
-                Board board = new Board();
-
-                JFrame mainFrame = new JFrame();
-                try {
-					mainFrame.add(board.drawBoard(null));
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+                Board board;
+				try {
+				    board = new Board();				
+	
+	                JFrame mainFrame = new JFrame();
+	
+	                mainFrame.add(board.drawBoard(null));
+	
+	                mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	                mainFrame.setLocationByPlatform(true);
+	                mainFrame.pack();
+	                mainFrame.setMinimumSize(new Dimension(BOARD_LENGTH, BOARD_LENGTH));
+	                mainFrame.setVisible(true);
+				} catch (Exception ex) {
+					System.out.println("Something went wrong!");
 				}
-                mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                mainFrame.setLocationByPlatform(true);
-                mainFrame.pack();
-                mainFrame.setMinimumSize(new Dimension(BOARD_LENGTH, BOARD_LENGTH));
-                mainFrame.setVisible(true);
             }
         };
         SwingUtilities.invokeLater(r);
